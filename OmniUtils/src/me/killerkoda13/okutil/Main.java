bfeb6fe3ac3336cc47a8161e13775bd5cd54a832
killerkoda13 <killerkoda13@gmail.com>
2016-05-21 17:16:28 -0400

    More updates

--- OmniUtils/src/me/killerkoda13/okutil/Main.java
+++ OmniUtils/src/me/killerkoda13/okutil/Main.java
@@ -1,12 +1,23 @@
 package me.killerkoda13.okutil;
 
 import me.killerkoda13.okutil.Utilities.WorldUtils;
+import net.md_5.bungee.api.ChatColor;
+import net.minecraft.server.v1_9_R2.Packet;
+import net.minecraft.server.v1_9_R2.PacketDataSerializer;
+import net.minecraft.server.v1_9_R2.PacketListener;
+import net.minecraft.server.v1_9_R2.PacketPlayInKeepAlive;
 
 import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.v1_9_R2.entity.CraftPlayer;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.inventivetalent.packetlistener.PacketListenerAPI;
+import org.inventivetalent.packetlistener.handler.PacketHandler;
+import org.inventivetalent.packetlistener.handler.ReceivedPacket;
+import org.inventivetalent.packetlistener.handler.SentPacket;
 
 public class Main extends JavaPlugin{
 
@@ -14,6 +25,18 @@ public class Main extends JavaPlugin{
 	public void onEnable()
 	{
 
+		PacketListenerAPI.addPacketHandler(new PacketHandler() {
+
+            @Override
+            public void onSend(SentPacket packet) {
+            }
+
+            @Override
+            public void onReceive(ReceivedPacket packet) {
+            }
+
+        });
+		
 	}
 
 	@Override
@@ -21,14 +44,20 @@ public class Main extends JavaPlugin{
 	{
 
 	}
-
+	
+	
+	/**
+	 * Permissions
+	 */
+	Permission lag = new Permission("okutils.lag");
+	Permission entityfinder = new Permission("okutils.entityfinder");
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
 	{
 		Player player = (Player) sender;
 		if(cmd.getName().equalsIgnoreCase("okutils"))
 		{
-			if(player.hasPermission("okutils.entityfinder"))
+			if(player.hasPermission(entityfinder))
 			{
 				if(args[0].equalsIgnoreCase("ef") || args[0].equalsIgnoreCase("entityfinder"))
 				{
@@ -42,6 +71,18 @@ public class Main extends JavaPlugin{
 						WorldUtils.getEntitiesByChunk(player.getWorld(), 50, player);
 					}
 				}
+			}
+		}else if(cmd.getName().equalsIgnoreCase("lag"))
+		{
+			if(player.hasPermission(lag))
+			{
+				CraftPlayer craftplayer = (CraftPlayer) player;
+				int ping = craftplayer.getHandle().ping;
+
+					player.sendMessage(ChatColor.GRAY+"Ping profile");
+				
+					System.out.println("Ping:"+craftplayer.getHandle().ping);
+				
 			}
 		}
 		return true;
--- OmniUtils/src/me/killerkoda13/okutil/Utilities/WorldUtils.java
+++ OmniUtils/src/me/killerkoda13/okutil/Utilities/WorldUtils.java
@@ -46,21 +46,17 @@ public class WorldUtils {
 			{
 				ArrayList<Entity> ents = WorldUtils.Entities.get(e.getLocation().getChunk());
 				ents.add(e);
-				WorldUtils.Entities.put(e.getLocation().getChunk(),ents);
-				
+				WorldUtils.Entities.put(e.getLocation().getChunk(),ents);	
 			}else
 			{
 				ArrayList<Entity> ents = new ArrayList<Entity>();
 				ents.add(e);
 				WorldUtils.Entities.put(e.getLocation().getChunk(), ents);
 			}
-			
 		}
 		
 		for(Chunk c : WorldUtils.Entities.keySet())
-		{	
-
-				
+		{		
 			if(WorldUtils.Entities.get(c).size() >= check)
 			{
 				player.sendMessage("X:"+c.getEntities()[0].getLocation().getBlockX()+" Y: "+c.getEntities()[0].getLocation().getBlockY()+" Z:"+c.getEntities()[0].getLocation().getBlockZ() + " e: "+WorldUtils.Entities.get(c).size());
