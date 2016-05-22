3b817ab980f3693d3ba4c05fed0444036facb65b
killerkoda13 <killerkoda13@gmail.com>
2016-05-21 22:40:22 -0400

    Testing new IDE with github

--- OmniUtils/src/me/killerkoda13/okutil/Main.java
+++ OmniUtils/src/me/killerkoda13/okutil/Main.java
@@ -2,10 +2,7 @@ package me.killerkoda13.okutil;
 
 import me.killerkoda13.okutil.Utilities.WorldUtils;
 import net.md_5.bungee.api.ChatColor;
-import net.minecraft.server.v1_9_R2.Packet;
-import net.minecraft.server.v1_9_R2.PacketDataSerializer;
-import net.minecraft.server.v1_9_R2.PacketListener;
-import net.minecraft.server.v1_9_R2.PacketPlayInKeepAlive;
+
 
 import org.bukkit.GameMode;
 import org.bukkit.command.Command;
@@ -14,28 +11,12 @@ import org.bukkit.craftbukkit.v1_9_R2.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.inventivetalent.packetlistener.PacketListenerAPI;
-import org.inventivetalent.packetlistener.handler.PacketHandler;
-import org.inventivetalent.packetlistener.handler.ReceivedPacket;
-import org.inventivetalent.packetlistener.handler.SentPacket;
 
 public class Main extends JavaPlugin{
 
 	@Override
 	public void onEnable()
-	{
-
-		PacketListenerAPI.addPacketHandler(new PacketHandler() {
-
-            @Override
-            public void onSend(SentPacket packet) {
-            }
-
-            @Override
-            public void onReceive(ReceivedPacket packet) {
-            }
-
-        });
+    {
 		
 	}
 
@@ -51,6 +32,7 @@ public class Main extends JavaPlugin{
 	 */
 	Permission lag = new Permission("okutils.lag");
 	Permission entityfinder = new Permission("okutils.entityfinder");
+
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
 	{
